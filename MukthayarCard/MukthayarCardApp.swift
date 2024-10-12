89bc832c3044314bbab458152b647c91f2981ac9
Muhammad Mukthayar <64356624+Mukthayar1@users.noreply.github.com>
2024-10-12 15:00:06 +0500

    Initial Commit

---
+++ MukthayarCard/MukthayarCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  MukthayarCardApp.swift
+//  MukthayarCard
+//
+//  Created by Muhammd Mukthayar on 12/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct MukthayarCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MukthayarCardTests/MukthayarCardTests.swift
@@ -0,0 +1,17 @@
+//
+//  MukthayarCardTests.swift
+//  MukthayarCardTests
+//
+//  Created by Muhammd Mukthayar on 12/10/2024.
+//
+
+import Testing
+@testable import MukthayarCard
+
+struct MukthayarCardTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
